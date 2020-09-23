663b91bcfa1ebb6e5690a15fcae87efd7281e53d
Eemeli Welling <eemelilari@gmail.com>
2020-09-23 11:56:57 +0300

    init scripts

---
+++ GoTo duck in google.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         GoTo duck in google
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com/search*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var input_field_element = document.getElementsByClassName("gLFyf gsfi")[0]
+    var search_string = input_field_element.value
+
+    var duck_url = "https://duckduckgo.com/?q=" + search_string
+
+
+    var new_element_font_size = input_field_element.clientHeight
+    var new_element_string = '<div style="font-size: '+new_element_font_size+'px;margin: auto;padding-right: 7px;"><a href="'+duck_url+'" >D</a></div>'
+
+    function createElementFromHTML(htmlString) {
+        var div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+
+        // Change this to div.childNodes to support multiple top-level nodes
+        return div.firstChild;
+    }
+    var new_element = createElementFromHTML(new_element_string)
+
+    var parent_element = document.getElementsByClassName("RNNXgb")[0]
+    parent_element.appendChild(new_element)
+
+})();
---
+++ GoTo google in duck.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         GoTo google in duck
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://duckduckgo.com/*q=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var input_field_element = document.getElementById("search_form_input")
+    var search_string = input_field_element.value
+
+    var search_url = "https://www.google.com/search?q=" + search_string
+
+    var parent_element = document.getElementById("search_form")
+    var parent_element_width = parent_element.clientWidth
+
+
+    var new_element_font_size = Math.floor(input_field_element.clientHeight * 0.75)
+    var new_element_string = '<div style="position:absolute; font-size: '+new_element_font_size+'px;'
+    +'text-align:center;height:'+input_field_element.clientHeight+'px;'
+    +'left:'+parent_element_width+'px;padding-left:7px;top:0;line-height:normal;">'
+    +'<a style="vertical-align: middle;" href="'+search_url+'" >G</a></div>'
+
+    function createElementFromHTML(htmlString) {
+        var div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+
+        // Change this to div.childNodes to support multiple top-level nodes
+        return div.firstChild;
+    }
+
+    var new_element = createElementFromHTML(new_element_string)
+
+    parent_element.appendChild(new_element)
+
+
+
+})();
